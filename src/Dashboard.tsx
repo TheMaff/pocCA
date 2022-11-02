7ef84714d31b3c367c6bcecb4168456d2848c8a8
Marcos Flores Flores <marcflor@emeal.nttdata.com>
2022-11-02 17:31:11 -0300

    first commit

---
+++ src/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { ComponentsProvider, Space, Span } from '@looker/components'
+import { ExtensionContext } from '@looker/extension-sdk-react'
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <div>Dashboard</div>
+    )
+  }
+}
+
+export default Dashboard
